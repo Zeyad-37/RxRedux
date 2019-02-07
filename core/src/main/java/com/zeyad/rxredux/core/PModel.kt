2ec5b0af1ad973c3d65b201d7076b2d919c99f6e
Zeyad-37 <zeyad.gasser@gmail.com>
2019-02-07 18:26:59 +0100

    removed isLoading var and using object type instead

--- core/src/main/java/com/zeyad/rxredux/core/PModel.kt
+++ core/src/main/java/com/zeyad/rxredux/core/PModel.kt
@@ -2,27 +2,23 @@ package com.zeyad.rxredux.core
 
 sealed class PModel<S> {
     abstract val event: BaseEvent<*>
-    abstract val isLoading: Boolean
     abstract val bundle: S
 
     override fun toString() = "stateEvent: $event"
 }
 
 data class LoadingState<S>(override val bundle: S,
-                           override val event: BaseEvent<*>,
-                           override val isLoading: Boolean = true) : PModel<S>() {
+                           override val event: BaseEvent<*>) : PModel<S>() {
     override fun toString() = "State: Loading, " + super.toString()
 }
 
 data class ErrorState<S>(val error: Throwable,
                          override val bundle: S,
-                         override val event: BaseEvent<*>,
-                         override val isLoading: Boolean = false) : PModel<S>() {
+                         override val event: BaseEvent<*>) : PModel<S>() {
     override fun toString() = "State: Error, ${super.toString()}, Throwable: $error"
 }
 
 data class SuccessState<S>(override val bundle: S,
-                           override val event: BaseEvent<*> = EmptyEvent,
-                           override val isLoading: Boolean = false) : PModel<S>() {
+                           override val event: BaseEvent<*> = EmptyEvent) : PModel<S>() {
     override fun toString() = "State: Success, ${super.toString()},  Bundle: $bundle"
 }
--- core/src/main/java/com/zeyad/rxredux/core/view/PModObserver.kt
+++ core/src/main/java/com/zeyad/rxredux/core/view/PModObserver.kt
@@ -2,6 +2,7 @@ package com.zeyad.rxredux.core.view
 
 import android.arch.lifecycle.Observer
 import com.zeyad.rxredux.core.ErrorState
+import com.zeyad.rxredux.core.LoadingState
 import com.zeyad.rxredux.core.PModel
 import com.zeyad.rxredux.core.SuccessState
 
@@ -10,14 +11,16 @@ class PModObserver<V : IBaseView<S>, S>(private val view: V,
 ) : Observer<PModel<S>> {
     override fun onChanged(uiModel: PModel<S>?) {
         uiModel?.apply {
-            view.toggleViews(isLoading, event)
-            if (!isLoading) {
-                when (this) {
-                    is ErrorState -> view.showError(errorMessageFactory.invoke(error, event), event)
-                    is SuccessState -> {
-                        view.setState(bundle)
-                        view.renderSuccessState(bundle)
-                    }
+            when (this) {
+                is LoadingState -> view.toggleViews(true, event)
+                is ErrorState -> {
+                    view.toggleViews(false, event)
+                    view.showError(errorMessageFactory.invoke(error, event), event)
+                }
+                is SuccessState -> {
+                    view.toggleViews(false, event)
+                    view.setState(bundle)
+                    view.renderSuccessState(bundle)
                 }
             }
         }
