6894a42e7b70c2150c31c0832b967ad423e48537
Zeyad-37 <zeyad.gasser@gmail.com>
2019-03-11 13:10:19 +0100

    StateObserver is setting the state before toggling the views and rendering the success state

--- core/src/main/java/com/zeyad/rxredux/core/view/PStateObserver.kt
+++ core/src/main/java/com/zeyad/rxredux/core/view/PStateObserver.kt
@@ -6,8 +6,8 @@ import com.zeyad.rxredux.core.SuccessState
 class PStateObserver<V : BaseView<S, *>, S>(private val view: V) : Observer<SuccessState<S>> {
     override fun onChanged(uiModel: SuccessState<S>?) {
         uiModel?.apply {
-            view.toggleViews(false, event)
             view.setState(bundle)
+            view.toggleViews(false, event)
             view.renderSuccessState(bundle)
         }
     }
