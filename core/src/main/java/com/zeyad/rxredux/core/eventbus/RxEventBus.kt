69323daebd730a7976df6096cee64c30f3078c38
Zeyad-37 <zeyad.gasser@gmail.com>
2019-10-23 12:34:11 +0200

    when calling observe does not create a new flowable

--- core/src/main/java/com/zeyad/rxredux/core/eventbus/RxEventBus.kt
+++ core/src/main/java/com/zeyad/rxredux/core/eventbus/RxEventBus.kt
@@ -7,9 +7,11 @@ import io.reactivex.subjects.PublishSubject
 internal class RxEventBus(private val rxBus: PublishSubject<Any> = PublishSubject.create(),
                           val backPressureStrategy: BackpressureStrategy = BackpressureStrategy.BUFFER) : IRxEventBus<Any> {
 
+    private val flowable = rxBus.toFlowable(backPressureStrategy)
+
     override fun send(t: Any) = rxBus.onNext(t)
 
-    override fun observe(): Flowable<Any> = rxBus.toFlowable(backPressureStrategy)
+    override fun observe(): Flowable<Any> = flowable
 
     companion object : SingletonHolder<BackpressureStrategy, IRxEventBus<Any>>({
         RxEventBus(backPressureStrategy = it)
