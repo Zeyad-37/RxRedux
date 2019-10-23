cdba655f0cb8adc61460274ff1b7bd59a9add011
Zeyad-37 <zeyad.gasser@gmail.com>
2019-10-23 14:40:50 +0200

    sharing bus stream

--- core/src/main/java/com/zeyad/rxredux/core/eventbus/RxEventBus.kt
+++ core/src/main/java/com/zeyad/rxredux/core/eventbus/RxEventBus.kt
@@ -7,7 +7,7 @@ import io.reactivex.subjects.PublishSubject
 internal class RxEventBus(private val rxBus: PublishSubject<Any> = PublishSubject.create(),
                           val backPressureStrategy: BackpressureStrategy = BackpressureStrategy.BUFFER) : IRxEventBus<Any> {
 
-    private val flowable = rxBus.toFlowable(backPressureStrategy)
+    private val flowable = rxBus.toFlowable(backPressureStrategy).share()
 
     override fun send(t: Any) = rxBus.onNext(t)
 
