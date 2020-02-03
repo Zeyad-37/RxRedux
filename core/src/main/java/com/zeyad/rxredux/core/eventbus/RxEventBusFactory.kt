361d4de15d6db6e6a08cba6fb7d216c1648adb2f
Zeyad-37 <zeyad.gasser@gmail.com>
2020-02-03 15:47:07 +0100

    remove bus

--- core/src/main/java/com/zeyad/rxredux/core/eventbus/RxEventBusFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.zeyad.rxredux.core.eventbus
-
-import io.reactivex.BackpressureStrategy
-
-@Deprecated("Use your own implementation")
-object RxIntentBusFactory {
-    fun getInstance(backPressureStrategy: BackpressureStrategy = BackpressureStrategy.BUFFER):
-            IRxIntentBus<Any> = RxIntentBus.getInstance(backPressureStrategy)
-}
